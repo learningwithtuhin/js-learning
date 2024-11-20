a41682220db53ca2f8614ebb16603515d12a3219
learningwithtuhin <sarkartuhin071@gmail.com>
2024-11-20 09:30:32 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all js code as newer version
+//alert(3 + 3); //in node it will not work but in browser it will work
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+//suppose we are declaring the variable but not assiging the value it will be considered as a undefined
+//null means its an empty value. If we check the type of null in console it will show us as an object
+
+console.log(typeof null);
